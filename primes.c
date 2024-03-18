081916eaa11e9aa04b2711a3432547ff70234844
Luca Salluzzi <63552496+salluzziluca@users.noreply.github.com>
2024-03-18 17:33:54 -0300

    Crear el array de numeros

--- primes.c
+++ primes.c
@@ -1,7 +1,22 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+
 int
 main(int argc, char *argv[])
 {
-	// Your code here
+	int numero = atoi(argv[1]);
+	if (numero < 0) {
+		printf("no se aceptan numeros menores a 0");
+	}
+	int *numeros = malloc(sizeof(int) * numero);
+	for (int i = 0; i < numero; i++) {
+		numeros[i] = i + 2;
+		printf("%i \n", numeros[i]);
+	}
+	free(numeros);
+
 
 	return 0;
 }
