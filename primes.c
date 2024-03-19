f894fa71d910a9e542426895bb48f2ef84aec5db
Luca Salluzzi <63552496+salluzziluca@users.noreply.github.com>
2024-03-19 17:49:25 -0300

    hacer que pase el test de 10

--- primes.c
+++ primes.c
@@ -5,48 +5,76 @@
 #include <unistd.h>
 
 #define FD_READ 0   // file descriptrior de lectura
-#define FD_WRITE 1  // file descriptor de escritura
+#define FD_WRITE 1  // file descriptor de escritur
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define READ_END 0
+#define WRITE_END 1
+
+void
+create_filter(int read_fd)
+{
+	int prime;
+	if (read(read_fd, &prime, sizeof(int)) <= 0) {
+		// No more primes
+		return;
+	}
+
+	printf("primo %i \n", prime);
+	fflush(stdout);
+
+	int fd[2];
+	pipe(fd);
+	pid_t pid = fork();
+
+	if (pid == 0) {
+		// Child
+		close(fd[WRITE_END]);
+		create_filter(fd[READ_END]);
+		exit(0);
+	}
+
+	// Parent
+	close(fd[READ_END]);
+
+	int num;
+	while (read(read_fd, &num, sizeof(int)) > 0) {
+		if (num % prime != 0) {
+			write(fd[WRITE_END], &num, sizeof(int));
+		}
+	}
+
+	close(fd[WRITE_END]);
+	wait(2);
+}
 
 int
 main(int argc, char *argv[])
 {
-	int numero = atoi(argv[1]);
-	if (numero < 0) {
-		printf("no se aceptan numeros menores a 0");
-	}
+	int max = atoi(argv[1]);
 
-	int filedescs_padre_hijo[2];
-	int filedescs_hijo_padre[2];
-	pipe(filedescs_padre_hijo);
-	pipe(filedescs_hijo_padre);
-
+	int fd[2];
+	pipe(fd);
 	pid_t pid = fork();
-
 	if (pid == 0) {
-		close(filedescs_padre_hijo[FD_WRITE]);
-		int numero_filtro;
-		int numero_actual;
-		if (numero_actual % numero_filtro != 0) {
-		}
-		read(filedescs_padre_hijo[FD_READ],
-		     &numero_filtro,
-		     sizeof(numero_filtro));
-		printf("primo: %i \n", numero_filtro);
-
-		// hijo
-		// usa el primer numero que le llega como filtro (ese numero que le llega tmb es primo)
-	} else {
-		for (int i = 0; i < numero; i++) {
-			close(filedescs_padre_hijo[FD_READ]);
-			int numero_actual = i + 2;
-			if ((numero_actual) % 1 == 0) {
-				write(filedescs_padre_hijo[FD_WRITE],
-				      &numero_actual,
-				      sizeof(numero_actual));
-			}
-
-			return 0;
-		}
-		// padre
+		// Child
+		close(fd[WRITE_END]);
+		create_filter(fd[READ_END]);
+		exit(0);
 	}
+
+	// Parent
+	close(fd[READ_END]);
+
+	for (int i = 2; i <= max; i++) {
+		write(fd[WRITE_END], &i, sizeof(int));
+	}
+
+	close(fd[WRITE_END]);
+	wait(2);
+	return 0;
 }
